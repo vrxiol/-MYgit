df7cfb689c92fdb2717a38cf966aa5a46e7b64a0
vrxiol <46449646+vrxiol@users.noreply.github.com>
2025-03-04 11:06:38 +0700

    Update home.py

--- home.py
+++ home.py
@@ -6,3 +6,9 @@ st.header("🍖🍖Website Developing using Python🍖🍖")
 
 st.image('./ing/mayorma.jpg')
 st.subheader("Dog")
+
+html_7 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
